7133d8b164655570e46cb4cfbec46b4cdf44a9ad
Peter Bosch <me@pbos.ch>
2024-12-23 01:26:29 +0100

    Fixes to alkqsio

--- src/dpm/alk/alkqsio.v
+++ src/dpm/alk/alkqsio.v
@@ -1,4 +1,5 @@
 `include "ucodedef.vh"
+`include "chipmacros.vh"
 
 /*********************************************************************
  * Project: COMET / DEC VAX-11/750
@@ -22,21 +23,20 @@
  
 module alkqsio(
 	/* DSIZE field */
-	input [1:0] dsize_l,
+	input [1:0] dsize_h,
 	
 	/* DQ field decode outputs */
-	input  dq_q_shl_h,
-	input  dq_q_shr_h,
+	input  dq_q_shl_l,
+	input  dq_q_shr_l,
 	
 	/* ALUSHF field decode outputs */
-	input  alushf_force_sout0_l, /* ALUSHF dictates 0 -> shift out  */
+	input  alushf_force_sout0_h, /* ALUSHF dictates 0 -> shift out  */
 	
 	/* ALPCTL field decode outputs */
-	input  alpctl_div_divdbl_l,
 	input  alpctl_divdbl_l,
 	input  alpctl_mul_l,
-	input  alpctl_div_h_b,
-	input  alpctl_div_h,
+	input  alpctl_div_l,
+	input  alpctl_rem_l,
 	
 	/* Shift input (from Q shift mux) */
 	input  q_sin_h,
@@ -45,39 +45,41 @@ module alkqsio(
 	output q_sout_shl_h,
 	output q_sout_shr_h,
 	
-	/* Q_SIO[] pad inputs */
+	/* Q_SIO_L[] pad inputs ( polarity = pad polarity ) */
 	input  q_sio0_in_l,
 	input  q_sio7_in_l,
 	input  q_sio15_in_l,
 	input  q_sio31_in_l,
 	
-	/* Q_SIO[] pad outputs ( open drain, 1 means pull down ) */
+	/* Q_SIO_L[] pad outputs ( open drain, polarity = pad polarity ) */
 	output q_sio0_out_l,
 	output q_sio7_out_l,
 	output q_sio15_out_l,
 	output q_sio31_out_l );
 	
+	wire alpctl_div_divdbl_l = ~(~alpctl_div_l | ~alpctl_divdbl_l );
+	
 	// dsize_h / dsize_l was replaced by ^~ comparison!
 	
 	/* Input enable generation for Q_SIO[7]  */
 	/* Matches DSIZE = 10 when div_h_b is high */
-	wire q_shl8_en_l  = ~( alpctl_div_h_b & &(dsize_h[1:0] ^~ 2'b00) );
+	wire q_shl8_en_l  = ~(~alpctl_div_l   & &(dsize_h[1:0] ^~ 2'b00) );
 	wire q_shl8_en_h  = ~q_shl8_en_l;
 	
 	/* Input enable generation for Q_SIO[15] */
 	/* Matches DSIZE = 00 when div_h_b is high */
-	wire q_shl16_en_l = ~( alpctl_div_h_b & &(dsize_h[1:0] ^~ 2'b10) );
-	wire q_shl16_en_h = ~q_shl8_en_l;
+	wire q_shl16_en_l = ~(~alpctl_div_l   & &(dsize_h[1:0] ^~ 2'b10) );
+	wire q_shl16_en_h = ~q_shl16_en_l;
 	
 	/* Input enable generation for Q_SIO[31] */
 	/* Matches DSIZE = 1x when div_h is high */
 	/* Also active when div_h is low !!! */
-	wire q_shl32_en_l = ~( alpctl_div_h & dsize_h[1] );
-	wire q_shl32_en_h = (~q_shl8_en_l) | (~alpctl_div_h); /* DeMorgan applied */
+	wire q_shl32_en_l = ~(~alpctl_div_l & dsize_h[1] );
+	wire q_shl32_en_h = (~q_shl32_en_l) | alpctl_div_l; /* DeMorgan applied */
 
 	/* Output enable generation for Q_SIO[0] */
-	wire CELL_15_06_OUT = ~( dq_q_shl_h & alushf_force_sout0_l & alpctl_rem_l );
-	wire q_shl_en_h   = ~(CELL_15_06_OUT & alpctl_div_divdbl_l)
+	wire CELL_15_06_OUT = ~(~dq_q_shl_l & ~alushf_force_sout0_h & alpctl_rem_l );
+	wire q_shl_en_h   = ~(CELL_15_06_OUT & alpctl_div_divdbl_l);
 
 	/* Output enable generation for Q_SIO[7] */
 	wire q_shr8_en_l  = ~(~alpctl_mul_l  & &(dsize_h[1:0] ^~ 2'b00) );
@@ -98,16 +100,15 @@ module alkqsio(
 	*/
 	// Changed mul_h into ~mul_l
 	wire q_shr32_en_l = ~((~alpctl_mul_l & dsize_h[1] ) | 
-	                      ( alpctl_mul_l & dq_q_shr_h & alushf_force_sout0_l & alpctl_divdbl_l ));
+	                      ( alpctl_mul_l & ~dq_q_shr_l & ~alushf_force_sout0_h & alpctl_divdbl_l ));
 	wire q_shr32_en_h = ~q_shr32_en_l;
 	
 	/* Pad output logic for Q_SIO[31,15,7] */
 	/* Effectively this drives q_sin_h onto the correct Q_SIO */
-	assign q_sio0_out_l  = q_sin_h & q_shl_en_h  ; /* SHL  , sin->Q[0]  */
-	assign q_sio7_out_l  = q_sin_h & q_shr8_en_h ; /* SHR8 , sin->Q[7]  */
-	assign q_sio15_out_l = q_sin_h & q_shr16_en_h; /* SHR16, sin->Q[15] */
-	assign q_sio31_out_l = q_sin_h & q_shr32_en_h; /* SHR32, sin->Q[31] */
-	
+	`OPENDRAIN_DRV( q_shl_en_h  , 1, q_sio0_out_l , ~q_sin_h ) /* SHL  , sin->Q[0]  */
+	`OPENDRAIN_DRV( q_shr8_en_h , 1, q_sio7_out_l , ~q_sin_h ) /* SHR8 , sin->Q[7]  */
+	`OPENDRAIN_DRV( q_shr16_en_h, 1, q_sio15_out_l, ~q_sin_h ) /* SHR16, sin->Q[15] */
+	`OPENDRAIN_DRV( q_shr32_en_h, 1, q_sio31_out_l, ~q_sin_h ) /* SHR32, sin->Q[31] */
 	
 	/* Pad wire-and logic for Q_SIO[31,15,7] */
 	/* Effectively this muxes the correct Q_SIO onto q_sout_shl_h */
