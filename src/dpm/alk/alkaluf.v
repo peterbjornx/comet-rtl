75cb6e6461ce9d036a134beadd33cf4df67d0768
Peter Bosch <me@pbos.ch>
2024-12-23 01:27:04 +0100

    Polarity and syntax fixes to alkaluf

--- src/dpm/alk/alkaluf.v
+++ src/dpm/alk/alkaluf.v
@@ -25,29 +25,30 @@ module alkaluf(
 	input alu_shift_op_l,       /* ALU op specifies A shift left or right */
 	
 	/* ALPCTL field decoded signals */
-	input alpctl_shl_op_l,      /* ALU op specifies A shift left */
-	input alpctl_shl_op_l,      /* ALU op specifies A shift right */
+	input alpctl_shl_op_h,      /* ALPCTL op specifies A shift left */
+	input alpctl_shr_op_h,      /* ALPCTL op specifies A shift right */
+	input alpctl_divdbl_l,
+	input alpctl_mul_group_h,
 	
 	/* Shift inputs (from ALU shifter routing) */
 	input  alu_sout_shl_h,
 	input  alu_sout_shr_h,
 	
-	output aluso_flag,
-	);
+	output aluso_flag_h );
 	
 	reg aluso_ff_q_h;
 	
 	/* ALUSO input mux */
 	wire aluso_ff_d_l = 
-		~(aluso_ff_q_h   & alpctl_mul_group_l & alu_shift_op_l    ) &
+		~(aluso_ff_q_h   & ~alpctl_mul_group_h & alu_shift_op_l    ) &
 		~(aluso_ff_q_h   & ~alpctl_divdbl_l                       ) &
-		~(alu_sout_shl_h &  alpctl_divdbl_l   & alpctl_shl_op_h   ) &
-		~(alu_sout_shr_h &                      alpctl_shr_op_h   );
+		~(alu_sout_shl_h &  alpctl_divdbl_l    & alpctl_shl_op_h   ) &
+		~(alu_sout_shr_h &                       alpctl_shr_op_h   );
 	
 	wire aluso_ff_d_h = ~aluso_ff_d_l;
 	
 	/* Clock ALUF on negative edge of QDCLK */
-	`FF_P( qdclk_l, aluso_ff_d_h, aluso_ff_q_h );
+	`FF_P( qdclk_l, aluso_ff_d_h, aluso_ff_q_h )
 	
 	assign aluso_flag_h = aluso_ff_q_h;
 	
