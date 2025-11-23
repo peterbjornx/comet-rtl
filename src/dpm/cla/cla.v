6b095aea56fc07a5e2f35f291acf58540b92dadc
Peter Bosch <me@pbos.ch>
2025-11-23 17:43:18 +0100

    CLA: Correct bus widths and mux sense

--- src/dpm/cla/cla.v
+++ src/dpm/cla/cla.v
@@ -32,21 +32,21 @@ module dc612_cla(
 
     output      muxo_l );
     
-    wire [8:0] pin_h = ~p_l;
-    wire [8:0] gin_h = ~g_a_l & ~g_b_l; //TODO: G_A/G_B? assuming wire-AND of inverting io pads
+    wire [7:0] pin_h = ~p_l;
+    wire [7:0] gin_h = ~g_a_l & ~g_b_l; //TODO: G_A/G_B? assuming wire-AND of inverting io pads
 
     wire [8:0] cbus_h;
 
     /* Carry lookahead circuit */
-    wire [8:0] ort0_h = {8{~ci_l    }} & {&pin_h[7:0],&pin_h[6:0],&pin_h[5:0],&pin_h[4:0],&pin_h[3:0],&pin_h[2:0],&pin_h[1:0], pin_h[0], 1'b1 };
-    wire [8:0] ort1_h = {8{gin_h [0]}} & {&pin_h[7:1],&pin_h[6:1],&pin_h[5:1],&pin_h[4:1],&pin_h[3:1],&pin_h[2:1], pin_h[1]  , 1'b1    , 1'b0 };
-    wire [8:0] ort2_h = {8{gin_h [1]}} & {&pin_h[7:2],&pin_h[6:2],&pin_h[5:2],&pin_h[4:2],&pin_h[3:2], pin_h[2]  , 1'b1      , 2'b00};
-    wire [8:0] ort3_h = {8{gin_h [2]}} & {&pin_h[7:3],&pin_h[6:3],&pin_h[5:3],&pin_h[4:3], pin_h[3]  , 1'b1      , 3'b000};
-    wire [8:0] ort4_h = {8{gin_h [3]}} & {&pin_h[7:4],&pin_h[6:4],&pin_h[5:4], pin_h[4]  , 1'b1      , 4'b0000};
-    wire [8:0] ort5_h = {8{gin_h [4]}} & {&pin_h[7:5],&pin_h[6:5], pin_h[5]  , 1'b1      , 5'b0000};
-    wire [8:0] ort6_h = {8{gin_h [5]}} & {&pin_h[7:6], pin_h[5]  , 1'b1      , 6'b0000};
-    wire [8:0] ort7_h = {8{gin_h [6]}} & { pin_h[7]  , 1'b1      , 6'b0000};
-    wire [8:0] ort8_h =   {gin_h [7],7'b0000} ;
+    wire [8:0] ort0_h = {9{~ci_l    }} & {&pin_h[7:0],&pin_h[6:0],&pin_h[5:0],&pin_h[4:0],&pin_h[3:0],&pin_h[2:0],&pin_h[1:0], pin_h[0], 1'b1 };
+    wire [8:0] ort1_h = {9{gin_h [0]}} & {&pin_h[7:1],&pin_h[6:1],&pin_h[5:1],&pin_h[4:1],&pin_h[3:1],&pin_h[2:1], pin_h[1]  , 1'b1    , 1'b0 };
+    wire [8:0] ort2_h = {9{gin_h [1]}} & {&pin_h[7:2],&pin_h[6:2],&pin_h[5:2],&pin_h[4:2],&pin_h[3:2], pin_h[2]  , 1'b1      , 2'b00};
+    wire [8:0] ort3_h = {9{gin_h [2]}} & {&pin_h[7:3],&pin_h[6:3],&pin_h[5:3],&pin_h[4:3], pin_h[3]  , 1'b1      , 3'b000};
+    wire [8:0] ort4_h = {9{gin_h [3]}} & {&pin_h[7:4],&pin_h[6:4],&pin_h[5:4], pin_h[4]  , 1'b1      , 4'b0000};
+    wire [8:0] ort5_h = {9{gin_h [4]}} & {&pin_h[7:5],&pin_h[6:5], pin_h[5]  , 1'b1      , 5'b00000};
+    wire [8:0] ort6_h = {9{gin_h [5]}} & {&pin_h[7:6], pin_h[5]  , 1'b1      , 6'b000000};
+    wire [8:0] ort7_h = {9{gin_h [6]}} & { pin_h[7]  , 1'b1      , 7'b0000000};
+    wire [8:0] ort8_h =   {gin_h [7],8'b00000000} ;
     assign     cbus_h = ort0_h | ort1_h | ort2_h | ort3_h | ort4_h | ort5_h | ort6_h | ort7_h | ort8_h;
 
     assign co_l   = ~cbus_h[7:0];
@@ -58,6 +58,6 @@ module dc612_cla(
     assign fov_h = 1'b0;
  
     /* MUX functionality */
-    assign muxo_l = ~( muxs_h == 1'b1 ? muxb_h : muxa_h );
+    assign muxo_l = ~( muxs_h == 1'b0 ? muxb_h : muxa_h );
 
 endmodule
